b8fefd35de1fe3270769bc486711ff7fb597c0f7
Atahan <atahan012000@gmail.com>
2017-10-15 15:15:05 +0300

    no message

--- DotT/Assets/Scripts/GameSide/PlayerCodes/HeroController.cs
+++ DotT/Assets/Scripts/GameSide/PlayerCodes/HeroController.cs
@@ -10,17 +10,14 @@ public class HeroController : NetworkBehaviour {
 	public Vector3 movePos = Vector3.zero;
 	public Health attackTarget;
 
-	[HideInInspector]
 	public float attackSpeed = 1.2f;
-	float attackCounter = 0f;
-	[HideInInspector]
+	public float attackCounter = 0f;
 	public float attackDamage = 50f;
-	[HideInInspector]
-	public float attackrange = 3f;
+	public float attackrange = 15f;
 
-	enum MovementMode{stop, move, attackmove}
+	public enum MovementMode{stop, move, attackmove}
 
-	MovementMode mode;
+	public MovementMode mode;
 
 	PlayerSpawner spawn;
 	GameObject myHero;
@@ -37,7 +34,7 @@ public class HeroController : NetworkBehaviour {
 				RaycastHit hit; 
 				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
 				if (Physics.Raycast (ray, out hit, 100.0f)) { 
-					if (hit.transform.root.gameObject.tag == "Hero") {
+					if (hit.transform.root.gameObject.tag == "Hero" && hit.transform.root.gameObject.GetComponent<Health>()!= null) {
 						Attack (hit.transform.root.gameObject);
 						Stop ();
 						mode = MovementMode.attackmove;
@@ -75,13 +72,15 @@ public class HeroController : NetworkBehaviour {
 				break;
 			case MovementMode.attackmove:
 				if (attackTarget != null) {
-					if (Vector3.Distance (spawn.myHero.transform.position, movePos) < attackrange) {
+					if (Vector3.Distance (spawn.myHero.transform.position, attackTarget.transform.position) < attackrange) {
 						if (attackCounter <= 0) {
-							ShootProjectile ();
+							if(attackTarget != null)
+								ShootProjectile ();
 							attackCounter = 1f / attackSpeed;
 						} else {
 							attackCounter -= Time.deltaTime;
 						}
+						spawn.myHero.GetComponent<NavMeshAgent> ().enabled = false;
 					} else {
 						movePos = attackTarget.transform.position;
 						GetComponent<PlayerSpawner> ().myHero.GetComponent<HeroObjectRelay> ().movePos = movePos;
@@ -103,6 +102,7 @@ public class HeroController : NetworkBehaviour {
 
 	void Attack (GameObject target){
 		if (isServer) {
+			if(target != null)
 			attackTarget = target.GetComponent<Health>();
 		} else {
 			CmdAttack (target);
@@ -111,6 +111,7 @@ public class HeroController : NetworkBehaviour {
 
 	[Command]
 	void CmdAttack (GameObject target){
+		if(target != null)
 		attackTarget = target.GetComponent<Health>();
 	}
 
