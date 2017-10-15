d23f4db1020573f7f003c22da9e6c74a2710bfad
Berke <stardusth2@gmail.com>
2017-10-15 12:36:01 +0300

    no message

--- DotT/Assets/Scripts/GameSide/GameCodes/Health.cs
+++ DotT/Assets/Scripts/GameSide/GameCodes/Health.cs
@@ -1,113 +1,377 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.AI;
 using System.Collections;
 
 public class Health : NetworkBehaviour
 {
-    [SyncVar]
-    public bool canCastSpell = true;
-    [SyncVar]
-    public bool canAutoAttack = true;
+    //Blind
+    bool canAutoAttack = true;
+    float blindDuration = 0;
 
-    [SyncVar]
-    public int movementSpeed = 100;
-    [SyncVar]
-    public int slowAmount = 0;
-    
-    [SyncVar]
-    public int myHealth = 0;
-    [SyncVar]
-    public int physicalArmor = 0;
-    [SyncVar]
-    public int magicArmor = 0;
+    //Silence
+    bool canCastSpell = true;
+    float silenceDuration = 0;
 
-    public enum DamageType {physical, magic, real};
+    //Movement
+    float movementSpeed = 100;
 
-    public void Damage(int damage, DamageType damageType)
+    //Root
+    NavMeshAgent agent;
+    bool agentIsEnabled = true;
+    float rootDuration;
+
+    //Unstoppable
+    bool canTakeCC = true;
+    float unstopDuration = 0;
+
+    //Defensive Stats
+    float maximumHealth = 1000;
+    float currentHealth = 1000;
+    float physicalArmor = 0;
+    float magicalArmor = 0;
+
+    //Damage
+    public enum DamageType{physical, magical, real};
+    public enum RatioType{current, missing, maximum};
+
+    //Invulnerable
+    bool canTakeDamage = true;
+    float invulDuration = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //Blind
+        if(blindDuration > 0)
+        {
+            blindDuration -= Time.deltaTime;
+        }
+        else
+        {
+            RemoveBlind();
+        }
+
+        //Silence
+        if(silenceDuration > 0)
+        {
+            silenceDuration -= Time.deltaTime;
+        }
+        else
+        {
+            RemoveSilence();
+        }
+
+        //Root
+        if(rootDuration > 0)
+        {
+            rootDuration -= Time.deltaTime;
+        }
+        else
+        {
+            RemoveRoot();
+        }
+
+        //Agent
+        if(agentIsEnabled)
+        {
+            agent.enabled = true;
+        }
+        else
+        {
+            agent.enabled = false;
+        }
+
+        //Unstoppable
+        if(unstopDuration > 0)
+        {
+            unstopDuration -= Time.deltaTime;
+        }
+        else
+        {
+            RemoveUnstoppability();
+        }
+
+        //Invulnerable
+        if (invulDuration > 0)
+        {
+            invulDuration -= Time.deltaTime;
+        }
+        else
+        {
+            RemoveInvulnerability();
+        }
+    }
+
+    //Adds blind effect.
+    public void Blind(float duration)
+    {
+        if(blindDuration < duration && canTakeCC)
+        {
+            canAutoAttack = false;
+            blindDuration = duration;
+        }
+    }
+
+    //Removes blind effect.
+    void RemoveBlind()
+    {
+        canCastSpell = true;
+    }
+
+    //Adds silence effect.
+    public void Silence(float duration)
+    {
+        if(silenceDuration < duration && canTakeCC)
+        {
+            canCastSpell = false;
+            silenceDuration = duration;
+        }
+    }
+
+    //Removes silence effect.
+    void RemoveSilence()
+    {
+        canAutoAttack = true;
+    }
+
+    //Increases or decreases movement speed.
+    public void ModifyMovementSpeed(float ratio, float duration)
+    {
+        movementSpeed *= 100 + ratio;
+        StartCoroutine(RemoveMovementModifier(ratio, duration));
+    }
+
+    //Removes the movement modifier's effect.
+    IEnumerator RemoveMovementModifier(float ratio, float duration)
+    {
+
+        while (duration > 0 && !canTakeCC)
+        {
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+        movementSpeed /= 100 + ratio;
+    }
+
+    //Adds root effect.
+    public void Root(float duration)
+    {
+        if (rootDuration < duration && canTakeCC)
+        {
+            agentIsEnabled = false;
+            rootDuration = duration;
+        }
+    }
+
+    //Removes root effect.
+    void RemoveRoot()
+    {
+        agentIsEnabled = true;
+    }
+
+    //Makes unstoppable.
+    public void MakeUnstoppable(float duration)
+    {
+        if (unstopDuration < duration)
+        {
+            canTakeCC = false;
+            unstopDuration = duration;
+            RemoveBlind();
+            RemoveSilence();
+            RemoveRoot();
+        }
+    }
+
+    //Makes stoppable.
+    void RemoveUnstoppability()
+    {
+        canTakeCC = true;
+    }
+
+    //Adds a defensive stat modifier.
+    void AddDefensiveStatModifier(float amount, DamageType damageType)
     {
         switch (damageType)
         {
             case DamageType.physical:
-                myHealth -= damage * (100 - physicalArmor);
+                physicalArmor = (physicalArmor - 100) * (1 - amount / 100) + 100;
                 break;
 
-            case DamageType.magic:
-                myHealth -= damage * (100 - magicArmor);
+            case DamageType.magical:
+                magicalArmor = (magicalArmor - 100) * (1 - amount / 100) + 100;
                 break;
 
             case DamageType.real:
-                myHealth -= damage;
+                physicalArmor = (physicalArmor - 100) * (1 - amount / 100) + 100;
+                magicalArmor = (magicalArmor - 100) * (1 - amount / 100) + 100;
                 break;
         }
     }
 
-    public void Damage(int damage, DamageType damageType, float duration)
+    //Adds a defensive stat modifier for a limited time.
+    void AddDefensiveStatModifier(float amount, DamageType damageType, float duration)
     {
-        damage = (int)(damage / (2 * duration));
+        switch(damageType)
+        {
+            case DamageType.physical:
+                physicalArmor = (physicalArmor - 100) * (1 - amount / 100) + 100;
+                break;
 
-        StartCoroutine(DamageOverTime(damage, damageType, duration));
+            case DamageType.magical:
+                magicalArmor = (magicalArmor - 100) * (1 - amount / 100) + 100;
+                break;
+
+            case DamageType.real:
+                physicalArmor = (physicalArmor - 100) * (1 - amount / 100) + 100;
+                magicalArmor = (magicalArmor - 100) * (1 - amount / 100) + 100;
+                break;
+        }
+        StartCoroutine(RemoveDefensiveStatModifier(amount, damageType, duration));
     }
 
-    IEnumerator DamageOverTime(int damage, DamageType damageType, float duration)
+
+    //Removes the defensive stat modifier.
+    IEnumerator RemoveDefensiveStatModifier(float amount, DamageType damageType, float duration)
     {
         while (duration > 0)
         {
-            yield return new WaitForSeconds(0.5f);
-            switch (damageType)
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+        switch (damageType)
+        {
+            case DamageType.physical:
+                physicalArmor = (physicalArmor - 100) / (1 - amount / 100) + 100;
+                break;
+
+            case DamageType.magical:
+                magicalArmor = (magicalArmor - 100) / (1 - amount / 100) + 100;
+                break;
+
+            case DamageType.real:
+                physicalArmor = (physicalArmor - 100) / (1 - amount / 100) + 100;
+                magicalArmor = (magicalArmor - 100) / (1 - amount / 100) + 100;
+                break;
+        }
+    }
+
+    //Adds disarm effect.
+    //RemoveBlind and RemoveSilence removes disarm effect automatically.
+    public void Disarm(float duration)
+    {
+        Blind(duration);
+        Silence(duration);
+    }
+
+    //Adds stun effect.
+    //RemoveBlind, RemoveSilence, and RemoveRoot removes stun effect automatically.
+    public void Stun(float duration)
+    {
+        Blind(duration);
+        Silence(duration);
+        Root(duration);
+    }
+
+    //Deals damage.
+    public void Damage(float damage, DamageType damageType)
+    {
+        if(canTakeDamage)
+        {
+            switch(damageType)
             {
                 case DamageType.physical:
-                    myHealth -= damage * (100 - physicalArmor);
+                    currentHealth -= damage * (100 - physicalArmor);
                     break;
 
-                case DamageType.magic:
-                    myHealth -= damage * (100 - magicArmor);
+                case DamageType.magical:
+                    currentHealth -= damage * (100 - magicalArmor);
                     break;
 
                 case DamageType.real:
-                    myHealth -= damage;
+                    currentHealth -= damage;
                     break;
             }
         }
     }
 
-    public void Slow(int slowAmount, float duration)
+    //Deals damage over time.
+    public void Damage(float damage, DamageType damageType, float duration)
     {
-        movementSpeed *= 100 - slowAmount;
-        StartCoroutine(SlowDuration(slowAmount, duration));
+        damage /= 2 * duration;
+
+        StartCoroutine(DamageOverTime(damage, damageType, duration));
     }
 
-    IEnumerator SlowDuration(int slowAmount, float duration)
+    IEnumerator DamageOverTime(float damage, DamageType damageType, float duration)
     {
-        yield return new WaitForSeconds(duration);
-        movementSpeed /= 100 - slowAmount;
+        while(duration >= 0)
+        {
+            yield return new WaitForSeconds(0.5f);
+            duration -= 0.5f;
+            Damage(damage, damageType);
+        }
     }
 
-    public void Silence(float duration)
+    //Deals damage proportional to health.
+    public void Damage(float damage, DamageType damageType, RatioType ratioType)
     {
-        canCastSpell = false;
-        StartCoroutine(SilenceDuration(duration));
+        switch(ratioType)
+        {
+            case RatioType.current:
+                damage *= currentHealth;
+                break;
+
+            case RatioType.missing:
+                damage *= maximumHealth - currentHealth;
+                break;
+
+            case RatioType.maximum:
+                damage *= maximumHealth;
+                break;
+        }
+
+        Damage(damage, damageType);
     }
 
-    IEnumerator SilenceDuration(float duration)
+    //Deals damage proportional to health over time.
+    public void Damage(float damage, DamageType damageType, RatioType ratioType, float duration)
     {
-        yield return new WaitForSeconds(duration);
-        canCastSpell = true;
+        switch(ratioType)
+        {
+            case RatioType.current:
+                damage *= currentHealth;
+                break;
+
+            case RatioType.missing:
+                damage *= maximumHealth - currentHealth;
+                break;
+
+            case RatioType.maximum:
+                damage *= maximumHealth;
+                break;
+        }
+
+        Damage(damage, damageType, duration);
     }
 
-    public void Stun(float duration)
+    //Makes invulnerabe.
+    public void MakeInvulnerable(float duration)
     {
-        int i = movementSpeed;
-        movementSpeed = 0;
-        canCastSpell = false;
-        canAutoAttack = false;
-        StartCoroutine(StunDuration(duration, i));
+        if (invulDuration < duration)
+        {
+            canTakeDamage = false;
+            invulDuration = duration;
+        }
     }
 
-    IEnumerator StunDuration(float duration, int i)
+    //Makes vulnerable.
+    void RemoveInvulnerability()
     {
-        yield return new WaitForSeconds(duration);
-        movementSpeed = i;
-        canCastSpell = true;
-        canAutoAttack = true;
+        canTakeDamage = true;
     }
 }
