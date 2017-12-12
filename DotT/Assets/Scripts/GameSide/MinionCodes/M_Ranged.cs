64d878a220aa45e49d6aa9da333684ebec9eacc2
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2017-12-12 19:09:28 +0300

    Fixed stuff

---
+++ DotT/Assets/Scripts/GameSide/MinionCodes/M_Ranged.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Ranged : Minion {
+    float attackRange = 10f;
+    new public int damage = 10;
+	
+    void Attack()
+    {
+        attackFocus.gameObject.GetComponent<Health>().ModifyHealth(damage, Health.HpModType.trueDamage);
+    }
+}
